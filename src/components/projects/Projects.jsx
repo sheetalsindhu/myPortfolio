f7de9b4bb2a0c3e0608a7d1b597b4a9d8d274662
sheetalsindhu <sheetalsindhu99@gmail.com>
2021-11-06 00:19:58 +0530

    changes

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -3,7 +3,7 @@ import styles from "./Projects.module.css";
 function Projects({ data }) {
   return (
     <>
-      <div className={styles.projects_section}>
+      {/* <div className={styles.projects_section}>
         <div className={styles.project_title}>
           <h1>{data.title}</h1>
           <h5>{data.description}</h5>
@@ -12,6 +12,28 @@ function Projects({ data }) {
           <p>{data.tagOne}</p>
           <p>{data.tagTwo}</p>
         </div>
+      </div> */}
+
+      <div className={styles.projects_section}>
+        <button className={(styles.custom_btn, styles.btn)}>
+          <span>
+            <div className={styles.project_title}>
+              <h1>{data.title}</h1>
+              <h5>{data.description}</h5>
+            </div>
+          </span>
+          <span>
+            <div className={styles.project_title}>
+              <h1>{data.title}</h1>
+              <h5>{data.description}</h5>
+            </div>
+
+            <div className={styles.project_info}>
+              <p>{data.tagOne}</p>
+              <p>{data.tagTwo}</p>
+            </div>
+          </span>
+        </button>
       </div>
     </>
   );
