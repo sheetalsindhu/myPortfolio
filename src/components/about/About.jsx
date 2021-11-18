a9af93ca20306220e7e3006d0c1994d50d9c0fb3
sheetalsindhu <sheetalsindhu99@gmail.com>
2021-11-18 16:32:44 +0530

    mobile

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,31 +1,6 @@
 import styles from "./About.module.css";
-// import { VscArrowBoth } from "react-icons/vsc";
-// import gsap from "gsap";
-// import { gsap } from "react-gsap";
-// import SplitText from "gsap";
 
 function About() {
-  // gsap.registerPlugin(SplitText);
-
-  // var tl = gsap.timeline(),
-  //   mySplitText = new SplitText("#quote", { type: "words,chars" }),
-  //   chars = mySplitText.chars; //an array of all the divs that wrap each character
-
-  // gsap.set("#quote", { perspective: 400 });
-
-  // console.log(chars);
-
-  // tl.from(chars, {
-  //   duration: 0.8,
-  //   opacity: 0,
-  //   scale: 0,
-  //   y: 80,
-  //   rotationX: 180,
-  //   transformOrigin: "0% 50% -50",
-  //   ease: "back",
-  //   stagger: 0.01,
-  // });
-
   return (
     <>
       <div id="about" className={styles.about_cont}>
